01e374f7ccd5dcb0d376ac2535edb24f81a7effd
Otavio <olamberty@maisaedu.com>
2024-11-15 18:15:21 -0300

    feat: add traversal methods and improve search functionality in binary tree

--- binary_tree.py
+++ binary_tree.py
@@ -3,51 +3,86 @@ class TreeNode:
         self.value = value
         self.left = None
         self.right = None
-        
-    def PrintTree(self):
-      if self.left:
-         self.left.PrintTree()
-      print(self.value, end=' ')
-      if self.right:
-         self.right.PrintTree()
-        
+
     def insert(self, val):
-        if self.value:
-            if val < self.value:
-                if self.left is None:
-                    self.left = TreeNode(val)
-                else:
-                    self.left.insert(val)
-            elif val > self.value:
-                if self.right is None:
-                    self.right = TreeNode(val)
-                else:
-                    self.right.insert(val)
-        else:
-            self.value = val
-            
+        if val < self.value:
+            if self.left is None:
+                self.left = TreeNode(val)
+            else:
+                self.left.insert(val)
+        elif val > self.value:
+            if self.right is None:
+                self.right = TreeNode(val)
+            else:
+                self.right.insert(val)
+
     def search(self, val):
         if self.value == val:
             return True
         
         if val < self.value and self.left:
-            self.left.search(val)
+            return self.left.search(val)
+
         elif val > self.value and self.right:
-            self.right.search(val)
-        
+            return self.right.search(val)
+
         return False
-  
-# tests          
 
-# root = TreeNode(1)
-# root.insert(7)
+    def preorder_traversal(self, res=None):
+        if res is None:
+            res = []
+
+        res.append(self.value)
+
+        if self.left:
+            self.left.preorder_traversal(res)
+            
+        if self.right:
+            self.right.preorder_traversal(res)
+
+        return res
+
+    def inorder_traversal(self, res=None):
+        if res is None:
+            res = []
+
+        if self.left:
+            self.left.inorder_traversal(res)
+
+        res.append(self.value)
+
+        if self.right:
+            self.right.inorder_traversal(res)
+
+        return res
+
+    def postorder_traversal(self, res=None):
+        if res is None:
+            res = []
+
+        if self.left:
+            self.left.postorder_traversal(res)
+
+        if self.right:
+            self.right.postorder_traversal(res)
+
+        res.append(self.value)
+
+        return res
+
+
+# tests
+
+# root = TreeNode(5)
 # root.insert(3)
-# root.insert(2)
-# root.insert(5)
-# root.insert(4)
-# root.PrintTree()
-# print('')
-# print('has 1 in binary tree: ', root.search(1))
-# print('has 10 in binary tree: ', root.search(10))
+# root.insert(1)
+# root.insert(10)
+# root.insert(7)
+# root.insert(15)
+# print("has 12 in binary tree: ", root.search(12))
+# print("has 10 in binary tree: ", root.search(10))
+# print("preorder traversal: ", root.preorder_traversal())
+# print("inorder traversal: ", root.inorder_traversal())
+# print("postorder traversal: ", root.postorder_traversal())
 
-# tests          
+# tests
